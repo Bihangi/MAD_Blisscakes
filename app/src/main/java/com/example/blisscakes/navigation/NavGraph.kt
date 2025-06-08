b13294bc54bfa3c80bdcab377f7335f567742fb7
Bihangi <bihangi.yohansa@gmail.com>
2025-06-08 19:37:04 +0530

    Updated the code

--- app/src/main/java/com/example/blisscakes/navigation/NavGraph.kt
+++ app/src/main/java/com/example/blisscakes/navigation/NavGraph.kt
@@ -3,6 +3,8 @@ package com.example.blisscakes.navigation
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 import androidx.navigation.compose.composable
 import com.example.blisscakes.pages.*
 
@@ -14,9 +16,24 @@ fun NavGraph(navController: NavHostController) {
         composable(NavRoutes.Cart) { CartPage(navController) }
         composable(NavRoutes.Login) { LoginPage(navController) }
         composable(NavRoutes.Signup) { SignupPage(navController) }
-        composable(NavRoutes.Detail) { DetailScreen(navController) }
-        composable(NavRoutes.Dashboard) { UserDashboard(navController) }
-        composable(NavRoutes.Summary) { ProductSummaryScreen(navController)  }
+        composable(NavRoutes.Products) { Products(navController)  }
+        composable(
+            route = "${NavRoutes.Detail}/{id}/{name}/{price}/{description}",
+            arguments = listOf(
+                navArgument("id") { type = NavType.IntType },
+                navArgument("name") { type = NavType.StringType },
+                navArgument("price") { type = NavType.StringType },
+                navArgument("description") { type = NavType.StringType }
+            )
+        ) { backStackEntry ->
+            val id = backStackEntry.arguments?.getInt("id")
+            val name = backStackEntry.arguments?.getString("name")
+            val price = backStackEntry.arguments?.getString("price")
+            val description = backStackEntry.arguments?.getString("description")
+
+            DetailScreen(navController, id, name, price, description)
+        }
+
     }
 }
 
