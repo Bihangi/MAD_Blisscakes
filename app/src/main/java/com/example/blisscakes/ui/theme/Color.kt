014e5b69c884739619dde2d4103317ab88a6da6c
Bihangi <bihangi.yohansa@gmail.com>
2025-06-08 17:06:43 +0530

    Updated the code

--- app/src/main/java/com/example/blisscakes/ui/theme/Color.kt
+++ app/src/main/java/com/example/blisscakes/ui/theme/Color.kt
@@ -3,14 +3,13 @@ package com.example.blisscakes.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val PinkPrimary = Color(0xFFE91E63)
-val LightPink = Color(0xFFFFF1F4)
 val White = Color(0xFFFFFFFF)
 val DarkBackground = Color(0xFF121212)
-val LightGray = Color(0xFFEEEEEE)
 val PinkDark = Color(0xFFE7B9B2)
-val ButtonRed = Color(0xFFE57373)
-val OutlinePink = Color(0xFFBA6B6C)
 val TextDark = Color(0xFF2C2C2C)
 val TextLight = Color(0xFFFFFFFF)
 val BackgroundLight = Color(0xFFFDEDE9)
 val BackgroundDark = Color(0xFF2A2A2A)
+val LightPink = Color(0xFFFFE6E9)
+val SoftPink = Color(0xFFF8B9AF)
+val MutedRose = Color(0xFFE7939D)
