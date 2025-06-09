b0716f1bb62d151f59eef36fc55a889fa1df03a6
Bihangi <bihangi.yohansa@gmail.com>
2025-06-09 08:53:14 +0530

    Fixed Errors

--- app/src/main/java/com/example/blisscakes/components/DashboardScaffold.kt
+++ app/src/main/java/com/example/blisscakes/components/DashboardScaffold.kt
@@ -3,17 +3,16 @@ package com.example.blisscakes.components
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 
 @Composable
 fun DashboardScaffold(
     navController: NavHostController,
-    content: @Composable (modifier: Modifier) -> Unit
+    content: @Composable (innerPadding: PaddingValues) -> Unit
 ) {
     Scaffold(
         bottomBar = { BottomNavigationBar(navController = navController) }
     ) { innerPadding ->
-        content(Modifier.padding(innerPadding))
+        content(innerPadding)
     }
 }
